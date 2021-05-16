d37f976790139f27e4aa977fa6a75e7c4f425d42
Pranav Negandhi <pranav@vertika.tv>
2021-05-16 23:11:50 +0530

    Added autodoc comments to BlockShade.

--- src/Shades/BlockShade.cs
+++ src/Shades/BlockShade.cs
@@ -2,8 +2,16 @@ using SkiaSharp;
 
 namespace Notadesigner.Shades
 {
+    /// <summary>
+    /// Type of shade that will always fill with defined color without variation.
+    /// </summary>
     public class BlockShade : Shade
     {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="color">The colour value to apply for this shade.</param>
+        /// <param name="warpSize">How much warp noise is allowed to alter the mark in pixels.</param>
         public BlockShade(SKColor color, int warpSize = 0) : base(warpSize)
         {
             Color = color;
@@ -15,6 +23,7 @@ namespace Notadesigner.Shades
             set;
         }
 
+        /// <inheritdoc/>
         public override SKColor DetermineShade(SKPoint point)
         {
             return Color;
