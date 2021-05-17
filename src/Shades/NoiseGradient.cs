fa20f0767baed2594052dd689fd8e62c145e99fd
Pranav Negandhi <pranav@vertika.tv>
2021-05-17 14:23:08 +0530

    Added the NoiseGradient shade and its associated test class.

---
+++ src/Shades/NoiseGradient.cs
@@ -0,0 +1,67 @@
+﻿using SkiaSharp;
+
+namespace Notadesigner.Shades
+{
+    /// <summary>
+    /// Type of shade that will produce varying gradient based on noise fields.
+    /// </summary>
+    public class NoiseGradient : Shade
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="color">Central RGB color of shade. Defaults to black.</param>
+        /// <param name="warpSize">How much warp_noise is allowed to alter the mark in pixels. Defaults to 0.</param>
+        /// <param name="colorVariance">How much noise is allowed to affect the color from the central shade.</param>
+        public NoiseGradient(SKColor color, int warpSize = 0, int colorVariance = 70) : base(warpSize)
+        {
+            Color = color;
+            ColorVariance = colorVariance;
+            NoiseFields = new NoiseField[]
+            {
+                new NoiseField(),
+                new NoiseField(),
+                new NoiseField(),
+                new NoiseField()
+            };
+        }
+
+        public SKColor Color
+        {
+            get;
+            set;
+        }
+
+        public int ColorVariance
+        {
+            get;
+            set;
+        }
+
+        public NoiseField[] NoiseFields
+        {
+            get;
+            set;
+        }
+
+        /// <inheritdoc/>
+        public override SKColor DetermineShade(SKPoint point)
+        {
+            var noise = NoiseFields[0].Noise(point) - 0.5;
+            var colorAffect = noise * (2 * ColorVariance);
+            var red = Color.Red + colorAffect;
+
+            noise = NoiseFields[1].Noise(point) - 0.5;
+            colorAffect = noise * (2 * ColorVariance);
+            var green = Color.Green + colorAffect;
+
+            noise = NoiseFields[2].Noise(point) - 0.5;
+            colorAffect = noise * (2 * ColorVariance);
+            var blue = Color.Blue + colorAffect;
+
+            var alpha = Color.Alpha;
+
+            return ColorUtilities.ColorClamp(red, green, blue, alpha);
+        }
+    }
+}
---
+++ src/ShadesUnitTests/NoiseGradientUnitTests.cs
@@ -0,0 +1,198 @@
+﻿using NUnit.Framework;
+using SkiaSharp;
+using System.Collections.Generic;
+
+namespace Notadesigner.Shades.Tests
+{
+    public class NoiseGradientUnitTests : IDrawingUnitTests
+    {
+        private readonly IReadOnlyDictionary<int, SKColor> colorPoints = new Dictionary<int, SKColor>()
+        {
+            { 0, new SKColor(255, 0, 0, 16) },
+            { 50, new SKColor(0, 255, 0, 16) },
+            { 100, new SKColor(0, 0, 255, 16) }
+        };
+
+        [TestCase]
+        [Description("The method being tested does not return a value. The result of the graphic operation are saved to disk for manual verification.")]
+        public void CircleOutlineTest()
+        {
+            var canvas = Shade.Canvas(100, 100, SKColors.White);
+
+            var instance = new NoiseGradient(new SKColor(0, 255, 0, 16), 10, 70);
+            instance.CircleOutline(canvas, new SKPoint(50, 50), 40.0f);
+
+            using var data = canvas.Encode(SKEncodedImageFormat.Png, 80);
+            using var stream = System.IO.File.OpenWrite($"{nameof(NoiseGradientUnitTests)}-{nameof(CircleOutlineTest)}.png");
+            data.SaveTo(stream);
+
+            Assert.IsTrue(true);
+        }
+
+        [TestCase]
+        [Description("The method being tested does not return a value. The result of the graphic operation are saved to disk for manual verification.")]
+        public void CircleTest()
+        {
+            var canvas = Shade.Canvas(100, 100, SKColors.White);
+
+            var instance = new NoiseGradient(new SKColor(0, 255, 0, 16), 10, 70);
+            instance.Circle(canvas, new SKPoint(50, 50), 40.0f);
+
+            using var data = canvas.Encode(SKEncodedImageFormat.Png, 80);
+            using var stream = System.IO.File.OpenWrite($"{nameof(NoiseGradientUnitTests)}-{nameof(CircleTest)}.png");
+            data.SaveTo(stream);
+
+            Assert.IsTrue(true);
+        }
+
+        private void CircleTestImpl(GradientAxis axis)
+        {
+        }
+
+        [TestCase]
+        [Description("The method being tested does not return a value. The result of the graphic operation are saved to disk for manual verification.")]
+        public void FillTest()
+        {
+            var canvas = Shade.Canvas(100, 100, SKColors.White);
+
+            var instance = new NoiseGradient(new SKColor(0, 255, 0, 16), 10, 70);
+            instance.Fill(canvas);
+
+            using var data = canvas.Encode(SKEncodedImageFormat.Png, 80);
+            using var stream = System.IO.File.OpenWrite($"{nameof(NoiseGradientUnitTests)}-{nameof(FillTest)}.png");
+            data.SaveTo(stream);
+
+            Assert.IsTrue(true);
+        }
+
+        [TestCase]
+        [Description("The method being tested does not return a value. The result of the graphic operation are saved to disk for manual verification.")]
+        public void LineTest()
+        {
+            var canvas = Shade.Canvas(100, 100, SKColors.White);
+
+            var instance = new NoiseGradient(new SKColor(0, 255, 0, 16), 10, 70);
+            instance.Line(canvas, new SKPoint(0, 0), new SKPoint(100, 100), 2);
+
+            using var data = canvas.Encode(SKEncodedImageFormat.Png, 80);
+            using var stream = System.IO.File.OpenWrite($"{nameof(NoiseGradientUnitTests)}-{nameof(LineTest)}.png");
+            data.SaveTo(stream);
+
+            Assert.IsTrue(true);
+        }
+
+        [TestCase]
+        [Description("The method being tested does not return a value. The result of the graphic operation are saved to disk for manual verification.")]
+        public void PointTest()
+        {
+            var canvas = Shade.Canvas(100, 100, SKColors.White);
+
+            var instance = new NoiseGradient(new SKColor(0, 255, 0, 16), 10, 70);
+            instance.Point(canvas, new SKPoint(50, 50));
+
+            using var data = canvas.Encode(SKEncodedImageFormat.Png, 80);
+            using var stream = System.IO.File.OpenWrite($"{nameof(NoiseGradientUnitTests)}-{nameof(PointTest)}.png");
+            data.SaveTo(stream);
+
+            Assert.IsTrue(true);
+        }
+
+        [TestCase]
+        [Description("The method being tested does not return a value. The result of the graphic operation are saved to disk for manual verification.")]
+        public void RectangleTest()
+        {
+            var canvas = Shade.Canvas(100, 100, SKColors.White);
+
+            var instance = new NoiseGradient(new SKColor(0, 255, 0, 16), 10, 70);
+            instance.Rectangle(canvas, new SKPoint(25, 25), 50, 50);
+
+            using var data = canvas.Encode(SKEncodedImageFormat.Png, 80);
+            using var stream = System.IO.File.OpenWrite($"{nameof(NoiseGradientUnitTests)}-{nameof(RectangleTest)}.png");
+            data.SaveTo(stream);
+
+            Assert.IsTrue(true);
+        }
+
+        [TestCaseSource(typeof(TestDataSources), nameof(TestDataSources.GetShapeEdgeData))]
+        [Description("The method being tested does not return a value. The result of the graphic operation are saved to disk for manual verification.")]
+        public void ShapeOutlineTest(IList<SKPoint> edgePoints, Queue<SKPoint> expected, int testIndex)
+        {
+            var canvas = Shade.Canvas(100, 100, SKColors.White);
+
+            var instance = new NoiseGradient(new SKColor(0, 255, 0, 16), 10, 70);
+            instance.ShapeOutline(canvas, edgePoints);
+
+            using var data = canvas.Encode(SKEncodedImageFormat.Png, 80);
+            using var stream = System.IO.File.OpenWrite($"{nameof(NoiseGradientUnitTests)}-{nameof(ShapeOutlineTest)}-{testIndex:00}.png");
+            data.SaveTo(stream);
+
+            Assert.IsTrue(true);
+        }
+
+        [TestCaseSource(typeof(TestDataSources), nameof(TestDataSources.GetShapeEdgeData))]
+        [Description("The method being tested does not return a value. The result of the graphic operation are saved to disk for manual verification.")]
+        public void ShapeTest(IList<SKPoint> edgePoints, Queue<SKPoint> expected, int testIndex)
+        {
+            var canvas = Shade.Canvas(100, 100, SKColors.White);
+
+            var instance = new NoiseGradient(new SKColor(0, 255, 0, 16), 10, 70);
+            instance.Shape(canvas, edgePoints);
+
+            using var data = canvas.Encode(SKEncodedImageFormat.Png, 80);
+            using var stream = System.IO.File.OpenWrite($"{nameof(NoiseGradientUnitTests)}-{nameof(ShapeTest)}-{testIndex:00}.png");
+            data.SaveTo(stream);
+
+            Assert.IsTrue(true);
+        }
+
+        [TestCase]
+        [Description("The method being tested does not return a value. The result of the graphic operation are saved to disk for manual verification.")]
+        public void TriangleOutlineTest()
+        {
+            var canvas = Shade.Canvas(100, 100, SKColors.White);
+
+            var instance = new NoiseGradient(new SKColor(0, 255, 0, 16), 10, 70);
+            instance.TriangleOutline(canvas, new SKPoint(50, 25), new SKPoint(75, 75), new SKPoint(25, 75));
+
+            using var data = canvas.Encode(SKEncodedImageFormat.Png, 80);
+            using var stream = System.IO.File.OpenWrite($"{nameof(NoiseGradientUnitTests)}-{nameof(TriangleOutlineTest)}.png");
+            data.SaveTo(stream);
+
+            Assert.IsTrue(true);
+        }
+
+        [TestCase]
+        [Description("The method being tested does not return a value. The result of the graphic operation are saved to disk for manual verification.")]
+        public void TriangleTest()
+        {
+            var canvas = Shade.Canvas(100, 100, SKColors.White);
+
+            var instance = new NoiseGradient(new SKColor(0, 255, 0, 16), 10, 70);
+            instance.Triangle(canvas, new SKPoint(50, 25), new SKPoint(75, 75), new SKPoint(25, 75));
+
+            using var data = canvas.Encode(SKEncodedImageFormat.Png, 80);
+            using var stream = System.IO.File.OpenWrite($"{nameof(NoiseGradientUnitTests)}-{nameof(TriangleTest)}.png");
+            data.SaveTo(stream);
+
+            Assert.IsTrue(true);
+        }
+
+        [TestCase]
+        [Description("The method being tested does not return a value. The result of the graphic operation are saved to disk for manual verification.")]
+        public void WeightedPointTest()
+        {
+            var canvas = Shade.Canvas(100, 100, SKColors.White);
+
+            var instance = new NoiseGradient(new SKColor(0, 255, 0, 16), 10, 70);
+            instance.WeightedPoint(canvas, new SKPoint(50, 50), 5);
+
+            using (var data = canvas.Encode(SKEncodedImageFormat.Png, 100))
+            using (var stream = System.IO.File.OpenWrite($"{nameof(NoiseGradientUnitTests)}-{nameof(WeightedPointTest)}.png"))
+            {
+                data.SaveTo(stream);
+            }
+
+            Assert.IsTrue(true);
+        }
+    }
+}
