28e9ba7634a64ee750e4b40766a8f69b7539526c
Pranav Negandhi <pranav@vertika.tv>
2022-02-08 14:48:00 +0530

    Added more test cases to ColorUtilitiesTests so that each field in a SKColor instance is tested separately.

--- src/ShadesUnitTests/ColorUtilitiesTests.cs
+++ src/ShadesUnitTests/ColorUtilitiesTests.cs
@@ -27,14 +27,36 @@ namespace Notadesigner.Shades.Tests
             int red = 256;
             int green = 128;
             int blue = 18;
-            var expected = new SKColor(255, (byte)green, (byte)blue);
-            yield return new TestCaseData(red, green, blue).Returns(expected);
+            int alpha = 255;
+            var expected = new SKColor(255, (byte)green, (byte)blue, (byte)alpha);
+            yield return new TestCaseData(red, green, blue, alpha).Returns(expected);
+
+            red = 46;
+            green = 300;
+            blue = 78;
+            alpha = 255;
+            expected = new SKColor((byte)red, 255, (byte)blue, (byte)alpha);
+            yield return new TestCaseData(red, green, blue, alpha).Returns(expected);
+
+            red = 46;
+            green = 187;
+            blue = 290;
+            alpha = 255;
+            expected = new SKColor((byte)red, (byte)green, 255, (byte)alpha);
+            yield return new TestCaseData(red, green, blue, alpha).Returns(expected);
+
+            red = 46;
+            green = 129;
+            blue = 78;
+            alpha = 563;
+            expected = new SKColor((byte)red, (byte)green, (byte)blue, 255);
+            yield return new TestCaseData(red, green, blue, alpha).Returns(expected);
         }
 
         [TestCaseSource(typeof(ColorUtilitiesTests), nameof(ColorClampOverflowData))]
-        public SKColor ColorClampOverflowTest(int red, int green, int blue)
+        public SKColor ColorClampOverflowTest(int red, int green, int blue, int alpha)
         {
-            return Shades.ColorUtilities.ColorClamp(red, green, blue);
+            return ColorUtilities.ColorClamp(red, green, blue, alpha);
         }
 
         private static IEnumerable<TestCaseData> ColorClampFData()
@@ -49,7 +71,7 @@ namespace Notadesigner.Shades.Tests
         [TestCaseSource(typeof(ColorUtilitiesTests), nameof(ColorClampFData))]
         public SKColor ColorClampFTest(float red, float green, float blue)
         {
-            return Shades.ColorUtilities.ColorClamp(red, green, blue);
+            return ColorUtilities.ColorClamp(red, green, blue, 0.0f);
         }
     }
 }
